cf66da55d31622b4a51008ff12cd28d9a1f71952
masikura <32968280+masikura@users.noreply.github.com>
2017-10-21 15:58:49 +0200

    teoria z js

---
+++ js/app.js
@@ -0,0 +1,14 @@
+console.log("działa!")
+var myName = 'Ania';
+console.log('myName', myName);
+var girls = ['Asia', 'Sylwia', 'Agata', 'Ola'];
+console.log(girls[1]);
+var a = 4;
+var b = 6;
+if (b>a){
+console.log("hurra");
+}else{
+console.log("not");
+}
+
+for (var i = 0; i<=girls.length-1; i++){console.log(girls[i])};
